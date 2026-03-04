7c3cabde5cb9a39eba4bafd7d3cc4dceb99b7216
Felix Wolff <fe.wolff@posteo.net>
2026-03-04 17:00:53 +0100

    Add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::{fs::File, io::BufReader, path::Path};
+use std::{fs::File, io::BufReader, path::PathBuf};
 
-use anyhow::{Context, anyhow};
+use anyhow::{Context, bail};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "/etc/interception/fluent.json")]
+    config: PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
-    let configs = [
-        Path::new("/etc/interception/fluent.d/fluent.json"),
-        Path::new("/etc/interception/fluent.json"),
-    ];
+    let args = Args::parse();
 
-    let config = configs.iter().find(|path| path.is_file());
-    let Some(config) = config else {
-        return Err(anyhow!("no config found, tried: {:?}", configs));
-    };
-    let file = File::open(config).context("could not open config file")?;
+    if !args.config.is_file() {
+        bail!("file not found: {:?}", args.config);
+    }
+    let file = File::open(args.config).context("could not open config file")?;
     let reader = BufReader::new(file);
     let config = serde_json::from_reader(reader).context("could not parse config file")?;
 
